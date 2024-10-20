ec94513b81cfa6609d950495cd760dceaf947a87
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-10-20 11:43:36 +0545

    feat: setup swagger

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 // Used in both creation and update of category.
 export class CreateCategoryDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name: string;
