5bfd8c387e091a01f7c68da902adf6f85fadb6d1
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-10-24 20:10:10 +0545

    feat: unit test for user service

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,52 +1,105 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { QueryFailedError } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
-import { DatabaseModule } from 'src/database/database.module';
-import { User } from 'src/entities/user.entity';
+import { User, UserRole } from 'src/entities/user.entity';
+import * as argonUtils from 'src/utils/argon.utils';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let userRepository: Repository<User>;
+  const repositoryToken = getRepositoryToken(User);
+
+  const mockUserRepo = {
+    delete: jest.fn(),
+    findOneBy: jest.fn(),
+    save: jest.fn(),
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [DatabaseModule, TypeOrmModule.forFeature([User])],
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: repositoryToken,
+          useValue: mockUserRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
-  });
-
-  describe('findAll', () => {
-    it('should return an array of users', async () => {
-      const result = await service.findAll();
-      expect(result).toBeInstanceOf(Array);
-    });
-  });
-
-  describe('find by non-UUID', () => {
-    it('should throw an error if id is not a valid UUID', async () => {
-      expect.assertions(1);
-      try {
-        await service.findById('1');
-      } catch (error) {
-        expect(error).toBeInstanceOf(QueryFailedError);
-      }
-    });
-  });
-
-  describe('find by UUID', () => {
-    it('should return a user', async () => {
-      const result = await service.findById(
-        '462f9d1c-ca91-48c2-9789-2543c3f77f0e'
-      );
-      expect(result).toBeInstanceOf(User);
-    });
+    userRepository = module.get<Repository<User>>(repositoryToken);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('userRepository should be defined', () => {
+    expect(userRepository).toBeDefined();
+  });
+
+  it('should encode the password correctly', async () => {
+    //Arrange
+    jest.spyOn(argonUtils, 'hashPassword').mockResolvedValue('hashedPassword');
+
+    // Act
+    await service.register({
+      email: 'pCz0H@example.com',
+      password: 'password',
+      userName: 'username',
+      role: UserRole.CUSTOMER,
+    });
+
+    // Assert
+    expect(argonUtils.hashPassword).toHaveBeenCalledWith('password');
+  });
+
+  describe('user creation', () => {
+    it('should call the user repo.create with correct params', async () => {
+      await service.register({
+        email: 'pCz0H@example.com',
+        password: 'password',
+        userName: 'username',
+        role: UserRole.CUSTOMER,
+      });
+      expect(userRepository.create).toHaveBeenCalledWith({
+        userName: 'username',
+        email: 'pCz0H@example.com',
+        password: 'hashedPassword',
+        role: UserRole.CUSTOMER,
+      });
+    });
+
+    it('should create a new user and save to the database', async () => {
+      // Arrange
+      jest.spyOn(userRepository, 'create').mockReturnValue({
+        id: '1',
+        email: 'pCz0H@example.com',
+        userName: 'username',
+        role: UserRole.CUSTOMER,
+        password: 'hashedPassword',
+        orders: [],
+      });
+
+      await service.register({
+        email: 'pCz0H@example.com',
+        password: 'password',
+        userName: 'username',
+        role: UserRole.CUSTOMER,
+      });
+      // Assert
+      expect(userRepository.save).toHaveBeenCalledWith({
+        id: '1',
+        email: 'pCz0H@example.com',
+        userName: 'username',
+        role: UserRole.CUSTOMER,
+        password: 'hashedPassword',
+        orders: [],
+      });
+    });
   });
 });
