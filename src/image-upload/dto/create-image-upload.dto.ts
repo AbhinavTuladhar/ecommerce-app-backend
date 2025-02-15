d22d32eefc11b2254ad8dfae2a23b14778939e97
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2025-02-15 19:04:57 +0545

    feat: add file upload input in swagger

--- src/image-upload/dto/create-image-upload.dto.ts
+++ src/image-upload/dto/create-image-upload.dto.ts
@@ -1,1 +1,6 @@
-export class CreateImageUploadDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateImageUploadDto {
+  @ApiProperty({ type: 'string', format: 'binary' })
+  file: Express.Multer.File;
+}
